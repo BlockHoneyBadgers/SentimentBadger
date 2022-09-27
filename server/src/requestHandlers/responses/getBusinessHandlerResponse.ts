6550858894e9c164aff235fe5b494cfb3edfc7f2
Maciej Andrzejczuk <MaciejAndrzejczuk@hotmail.com>
2022-09-27 16:22:21 +0200

    extract review type from response type

--- server/src/requestHandlers/responses/getBusinessHandlerResponse.ts
+++ server/src/requestHandlers/responses/getBusinessHandlerResponse.ts
@@ -1,12 +1,14 @@
 import { SentimentType } from '../../enums/SentimentType';
 
+export type Review = {
+  content: string | null;
+  googleRating: number;
+  sentimentScore: SentimentType | null;
+};
+
 export type GetBusinessHandlerResponse = {
   googleDataId: string;
   googleRatingAvg: number;
   name: string;
-  reviews: {
-    content: string | null;
-    googleRating: number;
-    sentimentScore: SentimentType | null;
-  }[];
+  reviews: Review[];
 };
